ddefdfea9fbe6a2a9887996dc99800a52d5e327e
claurene <cladt.laurene@gmail.com>
2019-02-07 10:57:54 +0100

    ajout projet

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use App\Entity\Usager;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+
+        $usager = new Usager();
+        $usager->setNom("Chow");
+        $usager->setPrenom("Arty");
+        $usager->setAdresse("2 rue jeanne d'arc, 54000 Nancy");
+        $manager->persist($usager);
+
+        $manager->flush();
+    }
+}
